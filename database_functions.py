4335c57adc72941b1665149eb4a7e16bcd9a768f
FabSchilling <fabian-schilling@gmx.net>
2018-05-03 13:16:14 +0200

    vaulted status now stored in db.json, gitignore now filters all .json

--- database_functions.py
+++ database_functions.py
@@ -1,5 +1,6 @@
 import json
 import vault_functions
+import re
 from six.moves import urllib
 
 from tinydb import TinyDB, Query
@@ -26,9 +27,15 @@ def loadDataFromJSON(path):
 
 def saveRelicDataToDatabase(data):
     relic_Data = data["relicRewards"]
+    vaulted_Relics = getVaultedRelics(data)
+
+    tier = ["Lith", "Meso", "Neo", "Axi"]
     for relics in relic_Data:
         for item in relic_Data[relics]['rewards']:
-            db.insert({'tier': relic_Data[relics]['tier'], 'type': relic_Data[relics]['type'], 'name': item['name'], 'rarity': item['intact']['name']})
+            if relic_Data[relics]['type'] in vaulted_Relics[tier.index(relic_Data[relics]['tier'])]:
+                db.insert({'tier': relic_Data[relics]['tier'], 'type': relic_Data[relics]['type'], 'name': item['name'], 'rarity': item['intact']['name'], 'vaulted': True})
+            else:
+                db.insert({'tier': relic_Data[relics]['tier'], 'type': relic_Data[relics]['type'], 'name': item['name'], 'rarity': item['intact']['name'], 'vaulted': False})
 
 
 
@@ -43,5 +50,36 @@ def getListOfPartsFromItem(item):
 def getAllRelics():
     return db.all()
 
+
+def getVaultedRelics(data):
+    relic_data = getAllRelics()
+    not_vaulted_relics = [[], [], [], []]
+    vaulted_relics = relic_data
+    pattern = '\w*\s\w*\sRelic'
+    text = str(data)
+
+    for match in re.findall(pattern, text):
+        if match[:4] == 'Lith':
+            if match[5:7] in relic_data[0]:
+                not_vaulted_relics[0].append(match[5:7])
+        elif match[:4] == 'Meso':
+            if match[5:7] in relic_data[1]:
+                not_vaulted_relics[1].append(match[5:7])
+        elif match[:3] == 'Neo':
+            if match[4:6] in relic_data[2]:
+                not_vaulted_relics[2].append(match[4:6])
+        elif match[:3] == 'Axi':
+            if match[4:6] in relic_data[3]:
+                not_vaulted_relics[3].append(match[4:6])
+
+    not_vaulted_relics = [list(set(not_vaulted_relics[0])), list(set(not_vaulted_relics[1])), list(set(not_vaulted_relics[2])), list(set(not_vaulted_relics[3]))]
+
+    count = 0
+    for tier in not_vaulted_relics:
+        for typ in tier:
+            vaulted_relics[count].remove(typ)
+        count += 1
+
+    return vaulted_relics
 
 
